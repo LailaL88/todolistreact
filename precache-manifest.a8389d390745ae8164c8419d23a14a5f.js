69401cb78c418188f0a44567c487682396034ec6
Olga <olga_ze@inbox.lv>
2020-05-14 18:21:45 +0300

    Updates

---
+++ precache-manifest.a8389d390745ae8164c8419d23a14a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffd0e46fc10ec6b448f7c6e4e795dd4",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "cffa7385c479d9f18233",
+    "url": "/todolistreact/static/css/main.eef71d6d.chunk.css"
+  },
+  {
+    "revision": "600ed4c036055aff9a12",
+    "url": "/todolistreact/static/js/2.c412dff3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.c412dff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cffa7385c479d9f18233",
+    "url": "/todolistreact/static/js/main.ed57cee5.chunk.js"
+  },
+  {
+    "revision": "ae42f8015eea320c73a7",
+    "url": "/todolistreact/static/js/runtime-main.d7539e5f.js"
+  }
+]);
